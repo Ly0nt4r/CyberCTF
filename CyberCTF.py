77bd7b574671151f36735e152743d1ed20c27fd6
Javier G <87484792+Ly0nt4r@users.noreply.github.com>
2022-06-05 23:50:40 +0200

    Add files via upload

--- CyberCTF.py
+++ CyberCTF.py
@@ -15,6 +15,7 @@ requiredNamed.add_argument("-i","--ip", help="Option to put the ip", required=Tr
 parser.add_argument("-f","--fuzz", help="Usage fuzz")
 parser.parse_args()
 args=parser.parse_args()
+list=[]
 
 
 def banner():
@@ -22,16 +23,16 @@ def banner():
 	print ('''
 	 ██████╗██╗   ██╗██████╗ ███████╗██████╗      ██████╗████████╗███████╗
 	██╔════╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔══██╗    ██╔════╝╚══██╔══╝██╔════╝
-	██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝    ██║        ██║   █████╗  
-	██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗    ██║        ██║   ██╔══╝  
-	╚██████╗   ██║   ██████╔╝███████╗██║  ██║    ╚██████╗   ██║   ██║     
-	 ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝     ╚═════╝   ╚═╝   ╚═╝     
-					author: @Lyont4R | CyberCTF v.1.0.1
+	██║      ╚████╔╝ ██████╔╝█████╗  ██████╔╝    ██║        ██║   █████╗
+	██║       ╚██╔╝  ██╔══██╗██╔══╝  ██╔══██╗    ██║        ██║   ██╔══╝
+	╚██████╗   ██║   ██████╔╝███████╗██║  ██║    ╚██████╗   ██║   ██║
+	 ╚═════╝   ╚═╝   ╚═════╝ ╚══════╝╚═╝  ╚═╝     ╚═════╝   ╚═╝   ╚═╝
+					author: @Lyont4R | CyberCTF v1.0.1
 	----------------------------------------------------------------------
 	''')
 
 
-def udpEnumeration():
+def portsEnumerationUDP():
 	try:
 		nmU= nmap.PortScanner()
 		nmU.scan(hosts=args.ip, arguments="-sU -n --top-ports 300 -T5")
@@ -45,16 +46,16 @@ def udpEnumeration():
 			lport = nmU[host][proto].keys()
 			sorted(lport)
 		for port in lport:
-			print ('port : %s\tstate : %s' % (port, nmU[host][proto][port]['state']))
+			print ('port: %s\tstate: %s' % (port, nmU[host][proto][port]['state']))
 	except:
 		exit(1)
 
-def portsEnumeration():
+def portsEnumerationTCP():
 
 	try:
 		p2=log.progress("Scanning ports...")
 		nm = nmap.PortScanner()
-		nm.scan(hosts=args.ip, arguments="-Pn -n -p- --min-rate 5000")
+		nm.scan(hosts=args.ip, arguments="-Pn -n --min-rate 5000 ")
 		for host in nm.all_hosts():
 			print('----------------------------------------------------')
 			print('Host : %s (%s)' % (host, nm[host].hostname()))
@@ -65,16 +66,34 @@ def portsEnumeration():
 			lport = nm[host][proto].keys()
 			sorted(lport)
 		for port in lport:
-			print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
-		udpEnumeration()
+			print ('port: %s\tstate: %s \t\t | ' % (port, nm[host][proto][port]['state']))
 	except:
+		print ("[/!\] CAN'T CONNECT WITH THE HOST [/!\]")
 		exit(1)
 
-def scanVulnerability():
-	print ("continuar")
-
-
+def scanVulnerability(port):
+	match port:
+		case 21:
+			return "[!] FTP PORT: [!]"
+		case 80,443,8080:
+			return "[!] HTTP/S PORT:  SHOULD FUZZING (SUBDOMAIN's AND DIRECTORY's) & VISUALIZE VERSIONS [!]"
+		case 23:
+			return "[!] TELNET PORT: [!]"
+		case 25, 465, 587:
+			return "[!] SMTP PORT: [!]"
+		case 110, 995:
+			return "[!] POP3 PORT:  [!]" 
+		case 135, 593:
+			return "[!] MSRPC PORT: [!]"
+		case 139, 445:
+			return "[!] SMB PORT: [!]"
+		case 143, 993:
+			return "[!] IMAP PORT: [!]"
+		case 161, 162, 10161, 10162:
+			return "[!] SNMP PORT: [!]"
 
 if __name__=="__main__":
 	banner()
-	portsEnumeration()
+	portsEnumerationTCP()
+
+
